408e11b50918071d503351037c847345a65704ad
AkankshaAnil <akankshakodgirwar07@gmail.com>
2023-07-04 23:56:39 +0530

    [AkankshaAnil] adding functionality to check whether employee is present or absent

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -3,5 +3,18 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome To Employee Wage Computation!");
+
+        int IS_FULL_TIME = 0;
+        Random random = new Random();
+        int empCheck = random.Next(1,2);
+
+        if(empCheck == IS_FULL_TIME)
+        {
+            Console.WriteLine("Employee Is Present");
+        }
+        else
+        {
+            Console.WriteLine("Employee Is Absent");
+        }
     }
 }

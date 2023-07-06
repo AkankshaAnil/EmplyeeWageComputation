aee8c26d14d71e15bfc9c44f0e0973134b7e0e3b
AkankshaAnil <akankshakodgirwar07@gmail.com>
2023-07-06 08:47:24 +0530

    [AkankshaAnil] merging from UC1

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,10 +5,7 @@
         Console.WriteLine("Welcome To Employee Wage Computation!");
 
         int IS_FULL_TIME = 1;
-        int EMP_RATE_PER_HOUR = 20;
-
-        int empHour = 0;
-        int empWage = 0;
+        
 
         Random random = new Random();
         int empCheck = random.Next(0,2);
@@ -17,15 +14,15 @@
         if(empCheck == IS_FULL_TIME)
         {
             Console.WriteLine("Employee Is Present");
-            empHour = 9;
+            
         }
         else
         {
             Console.WriteLine("Employee Is Absent");
-            empWage = 0;
+            
         }
-        empWage += empHour * EMP_RATE_PER_HOUR;
-        Console.WriteLine("EmpWage :" +empWage);
+        
+        
 
     }
 }

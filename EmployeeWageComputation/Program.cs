0244cfd347ea39e396dc32415a94201845af5ac7
AkankshaAnil <akankshakodgirwar07@gmail.com>
2023-07-06 08:50:22 +0530

    [AkankshaAnil] merging from UC2

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,24 +5,16 @@
         Console.WriteLine("Welcome To Employee Wage Computation!");
 
         int IS_FULL_TIME = 1;
-        
-
         Random random = new Random();
         int empCheck = random.Next(0,2);
-        
         Console.WriteLine(empCheck);
         if(empCheck == IS_FULL_TIME)
         {
             Console.WriteLine("Employee Is Present");
-            
         }
         else
         {
             Console.WriteLine("Employee Is Absent");
-            
         }
-        
-        
-
     }
 }
